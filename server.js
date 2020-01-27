0afdfc2ac80ba3f54eb425b2ad87224c7dd4c14b
Maciej Dzik <dzikdrums@gmail.com>
2020-01-27 20:23:10 +0100

    heroku problems

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 app.use('/api', testimonialsRoutes);
-
-app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
